78ee3ffe53f021deee4428a7c1fc6b1910fcc53f
rakib bari <rakibbari@rakibs-MacBook-Pro.local>
2024-11-17 12:31:31 +0600

    dynamicaly loadad json data

--- src/Pages/Home/Slider.jsx
+++ src/Pages/Home/Slider.jsx
@@ -29,7 +29,7 @@ const Slider = () => {
       >
         {/* First Slide */}
         <SwiperSlide
-          className="relative  text-white flex items-center justify-center pt-64"
+          className="relative  text-white flex items-center justify-center lg:pt-64 pt-40"
           style={{
             backgroundImage: "url('/slider/house.jpg')",
             backgroundSize: "cover",
@@ -40,11 +40,11 @@ const Slider = () => {
           <div className="absolute inset-0 bg-black opacity-70"></div>
 
           <div className="relative text-center  ">
-            <div className="max-w-4xl mx-auto space-y-10">
-              <h1 className="mb-5 text-7xl font-extrabold">
+            <div className="max-w-4xl mx-auto lg:space-y-10 space-y-3">
+              <h1 className="mb-5 lg:text-7xl text-2xl font-extrabold">
                 Modern Family Home
               </h1>
-              <p className="text-lg">
+              <p className="lg:text-lg text-sm">
                 A modern house with a flat roof, sharp angles, and a mix of
                 concrete and metal finishes. Expansive floor-to-ceiling windows
                 flood the interior with natural light, highlighting the spacious
@@ -61,7 +61,7 @@ const Slider = () => {
 
         {/* Second Slide */}
         <SwiperSlide
-          className="relative h-screen text-white flex items-center justify-center pt-64"
+          className="relative h-screen text-white flex items-center justify-center lg:pt-64 pt-40"
           style={{
             backgroundImage: "url('/slider/townhouse.jpg')",
             backgroundSize: "cover",
@@ -72,11 +72,11 @@ const Slider = () => {
           <div className="absolute inset-0 bg-black opacity-70"></div>
 
           <div className="relative text-center">
-            <div className="max-w-5xl mx-auto space-y-10">
-              <h1 className="mb-5 text-7xl font-extrabold">
+            <div className="max-w-5xl mx-auto lg:space-y-10 space-y-3">
+              <h1 className="mb-5 lg:text-7xl text-2xl font-extrabold">
                 Downtown Apartment
               </h1>
-              <p className="mb-5 text-lg">
+              <p className="mb-5 lg:text-lg text-sm">
                 A chic downtown apartment featuring floor-to-ceiling windows
                 with stunning city views, modern fixtures, and an open-concept
                 living space. Sleek finishes like polished concrete floors and
@@ -93,7 +93,7 @@ const Slider = () => {
 
         {/* third Slide */}
         <SwiperSlide
-          className="relative h-screen text-white flex items-center justify-center pt-64"
+          className="relative h-screen text-white flex items-center justify-center lg:pt-64 pt-40"
           style={{
             backgroundImage: "url('/slider/apartment.jpg')",
             backgroundSize: "cover",
